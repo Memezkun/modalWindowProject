980572d2fd27b1c720fdffd82ba6c6544d0c7acf
Alexander <memezkun@gmail.com>
2021-08-16 16:00:42 +0300

    first commit

---
+++ setup/app.js
@@ -0,0 +1,18 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn = document.querySelector(".modal-btn");
+const modalOverlay = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+modalBtn.addEventListener("click", function () {
+  modalOverlay.style.visibility = "visible";
+  modalOverlay.style.zIndex = "0";
+});
+
+closeBtn.addEventListener("click", function () {
+  modalOverlay.style.visibility = "hidden";
+  modalOverlay.style.zIndex = "-10";
+});
